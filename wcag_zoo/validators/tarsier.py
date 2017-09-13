4516c001c1ce9813c9484cad298ba5a331e2079d
Samuel Spencer <theodore.therone@gmail.com>
2017-09-13 02:39:47 +0000

    add glowworm!

--- wcag_zoo/validators/tarsier.py
+++ wcag_zoo/validators/tarsier.py
@@ -8,8 +8,10 @@ class Tarsier(WCAGCommand):
     """
 
     animal = """
-        The tarsiers are prosimian (non-monkey) primates. They got their name from the long bones in their feet.
-        They are now placed in the suborder Haplorhini, together with the simians (monkeys).
+        The tarsiers are prosimian (non-monkey) primates. They got their name 
+        from the long bones in their feet.
+        They are now placed in the suborder Haplorhini, together with the 
+        simians (monkeys).
 
         Tarsiers have huge eyes and long feet, and catch the insects by jumping at them.
         During the night they wait quietly, listening for the sound of an insect moving nearby.
